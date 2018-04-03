af2121fdae5a9f30e07058d210652a505206f15d
Ellie Shuo Jin <ellieshuojin@utexas.edu>
2018-04-03 15:01:28 -0500

    Average of clustering coefficients for all nodes in various networks

---
+++ AverageClustering.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import numpy as np
+
+# loading network data
+G_LesMis = nx.read_gml('lesmis.gml')  # Les Miserables
+G_football = nx.read_gml('football.gml')  # Football network
+G_SP500 = nx.read_gexf('SP500.gexf')  # S&P500
+G_facebook = nx.read_edgelist('facebook_combined.edgelist')  # facebook
+G_power = nx.read_gml('power.gml', label='id')  # power grid
+G_fMRI = nx.read_adjlist('fMRI_HighRes.adjlist')  # fMRI network
+
+netName = ['Les Miserables',
+           'NCAA football',
+           'S&P 500',
+           'Facebook',
+           'Power grid',
+           'fMRI network']
+GList = [G_LesMis, G_football, G_SP500, G_facebook, G_power, G_fMRI]
+
+for name, G in zip(netName, GList):
+    print(name, nx.average_clustering(G))
